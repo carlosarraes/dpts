9d165c222935f6c1f377fa6495a64ea8e7785bcf
carraes <carraeshb@gmail.com>
2022-12-24 01:15:38 -0300

    feat: add map

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@
 import { Company } from './Company'
 import { User } from './User'
 
+const map: HTMLElement = document.getElementById('map') as HTMLElement
+
 const user = new User()
 const company = new Company()
 
 console.log(user)
 console.log(company)
+
+new google.maps.Map(map, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+})
