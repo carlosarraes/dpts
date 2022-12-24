8af4f22f6ba058e32bee22119321bd3bc7fd0569
carraes <carraeshb@gmail.com>
2022-12-24 10:50:04 -0300

    feat: complete

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,15 +1,39 @@
-const map: HTMLElement = document.getElementById('map') as HTMLElement
+interface Mappable {
+  location: {
+    lat: number
+    lng: number
+  }
+  markerContent(): string
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map
 
-  constructor() {
-    this.googleMap = new google.maps.Map(map, {
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId)!, {
       zoom: 1,
       center: {
         lat: 0,
         lng: 0,
       },
+    })
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    })
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      })
+
+      infoWindow.open(this.googleMap, marker)
     })
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-/// <reference types="@types/google.maps" />
 import { Company } from './Company'
 import { CustomMap } from './CustomMap'
 import { User } from './User'
 
 const user = new User()
 const company = new Company()
+const customMap = new CustomMap('map')
 
-new CustomMap()
+customMap.addMarker(user)
+customMap.addMarker(company)
